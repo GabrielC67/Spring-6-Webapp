0a93695f4932ca644bb54a350c3f5900ae6e4565
GabrielC67 <gabecruzpro@gmail.com>
2025-09-15 21:10:08 -0400

    checkpoint

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -1,4 +1,4 @@
-package webapp.springframework.spring_6_webapp;
+package guru.springframework.spring6webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.data.jdbc.JdbcDatabaseDialect;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -29,6 +33,13 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
 
+        Publisher scholasticBooks = new Publisher();
+        scholasticBooks.setPublisherName("Scholastic Books");
+        scholasticBooks.setAddress("1 Castor Avenue");
+        scholasticBooks.setCity("Wilmington");
+        scholasticBooks.setZipCode("19801");
+
+        Publisher sBSaved = publisherRepository.save(scholasticBooks);
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
 
@@ -46,10 +57,13 @@ public class BootstrapData implements CommandLineRunner {
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
 
-        System.out.println("In Bootstrap");
-        System.out.println("Author Count:" + authorRepository.count());
-        System.out.println("Book Count: " + bookRepository.count());
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
 
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
     }
 
