8a9f5de5f62d00f77f29c51f17e9911871c9795f
GabrielC67 <gabecruzpro@gmail.com>
2025-09-26 21:14:10 -0400

    Checkpoint

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -52,6 +52,8 @@ public class BootstrapData implements CommandLineRunner {
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
 
         Publisher scholasticBooks = new Publisher();
         scholasticBooks.setPublisherName("Scholastic Books");
